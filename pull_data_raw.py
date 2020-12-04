1db1e083bf43b2a7a72874538c56ef8e43ac55a6
rosskeim <43429420+rosskeim@users.noreply.github.com>
2020-12-04 08:24:59 -0700

    getting close on this one - just need to grab the win value'

--- pull_data_raw.py
+++ pull_data_raw.py
@@ -1,17 +1,12 @@
 import requests, json
 import pandas as pd
-from pandas.plotting import scatter_matrix
-from pandas.plotting import boxplot
-from tabulate import tabulate
-from pandas.io.json import json_normalize
 
 account = "bEEDcwc6ohmom9qBAJM7fHav5nSxp8H2E-Tfvf4iofQI0w"
-api_key = "RGAPI-1426ffcc-2ee9-4584-8a39-1a3baa3831bd"
+api_key = "RGAPI-1e5772b3-a66c-4cff-b7f3-7e12bea7b045"
 
 response = requests.get("https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + account + "?api_key=" + api_key)
 
 data = response.json()
-print(data)
 
 df = pd.DataFrame(data['matches'])
 games = df['gameId'].values.tolist()
@@ -20,21 +15,13 @@ record = []
 wins = 0
 losses = 0
 
-for x in range(5):
-	response = requests.get("https://na1.api.riotgames.com/lol/match/v4/matches/" + str(games[x]) + "?api_key=" + str(api_key))
-	data = response.json()
-	print(data)
-
-#response = requests.get("https://na1.api.riotgames.com/lol/match/v4/matches/" + str(matches[1]) + "?api_key=" + api_key)
-#data = response.json()
-
-#for p in data['participantIdentities']:
-#	if p['player']['currentAccountId'] == 'bEEDcwc6ohmom9qBAJM7fHav5nSxp8H2E-Tfvf4iofQI0w':
-#		part = p['participantId']
-
-#for y in data['participants']:
-#	if y['participantId'] == part:
-#		#result = y['win']
-#		print(result)
-
-#print(result)
+for x in range(1):
+    response = requests.get("https://na1.api.riotgames.com/lol/match/v4/matches/" + str(games[x]) + "?api_key=" + str(api_key))
+    data = response.json()
+    print(data.keys())
+    for y in data['participantIdentities']:
+        if(y['player']['currentAccountId'] == account):
+            part = y['participantId']
+    for z in data['participants']:
+        if z['participantId'] == part:
+            print(z['participantId']['stats']['win'])
